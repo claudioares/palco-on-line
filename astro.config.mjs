9645756a4d7f54d525a46dd8946643cb4c6bb408
claudioares <claudiojasoares.dev@gmail.com>
2024-03-02 08:44:12 -0300

    :feat adicionando componente de pesquisa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,25 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import preact from '@astrojs/preact';
+import solid from '@astrojs/solid-js';
+
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false,
-  })]
+  integrations: [
+    react({
+      include: ['**/react/*'],
+    }),
+    preact({
+      include: ['**/preact/*'],
+    }),
+    solid({
+      include: ['**/solid/*'],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    })
+  ]
 });
