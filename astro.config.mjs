7e54b0fdde4087f0ba702afe2d4c8cf721ab15f7
claudioares <claudiojasoares.dev@gmail.com>
2024-03-03 03:30:33 -0300

    :feat adicionando o começo do layout da tela de video

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import preact from '@astrojs/preact';
-import solid from '@astrojs/solid-js';
 
 
 import tailwind from "@astrojs/tailwind";
@@ -11,12 +9,6 @@ export default defineConfig({
   integrations: [
     react({
       include: ['**/react/*'],
-    }),
-    preact({
-      include: ['**/preact/*'],
-    }),
-    solid({
-      include: ['**/solid/*'],
     }),
     tailwind({
       applyBaseStyles: false,
