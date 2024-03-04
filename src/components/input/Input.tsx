30f5d6d660d7c63dd7f9c8b56306c8aee6d109b6
claudioares <claudiojasoares.dev@gmail.com>
2024-03-04 12:11:23 -0300

    feat: Adicionando tela de login

---
+++ src/components/input/Input.tsx
@@ -0,0 +1,16 @@
+type ChildrnInput = {
+    placeholder: string,
+    id: string
+}
+
+export default function Input ({placeholder, id}:ChildrnInput) {
+    return(
+        <input
+            type="text"
+            id={id}
+            className="block text-left w-56 h-2 p-4 text-sm text-cyan-900 border border-gray-300 rounded-lg bg-zinc-800 focus:ring-cyan-900 focus:border-cyan-900 dark:bg-gray-700 dark:border-cyan-900 dark:placeholder-cyan-900 dark:text-white dark:focus:ring-cyan-900 dark:focus:border-cyan-900"
+            placeholder={placeholder}
+            required
+        />
+    )
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,15 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    extend: {
+      backgroundImage: {
+        'bg-logo': "url('./public/logoSVG.svg')",
+      }
+    },
     fontFamily: {
       'Architects-Daughter': ['"Architects Daughter", cursive;'],
-      "Roboto": ['"Roboto", sans-serif;']
+      "Roboto": ['"Roboto", sans-serif;'],
+      "Dosis": ['"Dosis", sans-serif;']
     }
   }
 }
