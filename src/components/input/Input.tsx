7eb7d1f5373df207ef1843541a8cfef795bc6926
claudioares <claudiojasoares.dev@gmail.com>
2024-03-07 09:18:34 -0300

    feat: Adicionando layout iincial com videos ilustrativos

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -3,12 +3,12 @@ type ChildrnInput = {
     id: string
 }
 
-export default function Input ({placeholder, id}:ChildrnInput) {
+export default function InputComponentReact ({placeholder, id}:ChildrnInput) {
     return(
         <input
             type="text"
             id={id}
-            className="block text-left w-56 h-2 p-4 text-sm text-cyan-900 border border-gray-300 rounded-lg bg-zinc-800 focus:ring-cyan-900 focus:border-cyan-900 dark:bg-gray-700 dark:border-cyan-900 dark:placeholder-cyan-900 dark:text-white dark:focus:ring-cyan-900 dark:focus:border-cyan-900"
+            className="block text-left w-56 h-2 p-4 text-sm text-slate-50 font-Roboto font-light border border-gray-300 rounded-lg bg-zinc-800 focus:ring-cyan-900 focus:border-cyan-900 dark:bg-gray-700 dark:border-cyan-900 dark:placeholder-cyan-900 dark:text-white dark:focus:ring-cyan-900 dark:focus:border-cyan-900"
             placeholder={placeholder}
             required
         />
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,8 +2,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      backgroundOpacity: ['active'],
       backgroundImage: {
-        'bg-logo': "url('./public/logoSVG.svg')",
+        'bg-logo': "url('./logoSVG.svg')",
       }
     },
     fontFamily: {
