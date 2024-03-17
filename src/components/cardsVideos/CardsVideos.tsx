207ec2b78864b064d7148b7655a8fd05ac684c2b
claudioares <claudiojasoares.dev@gmail.com>
2024-03-17 06:02:21 -0300

    feat: Adicionando inplementações para tablet e mobile

--- src/components/cardsVideos/CardsVideos.tsx
+++ src/components/cardsVideos/CardsVideos.tsx
@@ -3,9 +3,15 @@ import ModalCllickCard from "./ModalClickCard";
 export default function CardsVideos () {
     return(
         <>
-            <div className="flex flex-wrap w-full h-auto gap-4 justify-center items-center">
+            <div className={`
+                flex flex-wrap w-full h-auto gap-4 justify-center items-center
+                max-md:flex-col
+            `}>
                 {Array.from({length:10}).map((_, index)=> (
-                    <div className="w-[25%] h-72 flex flex-col gap-3 cursor-pointer" key={index}>
+                    <div className={`
+                        w-[25%] h-72 flex flex-col gap-3 cursor-pointer
+                        max-md:w-full max-md:px-4
+                    `} key={index}>
                         <div className="w-full h-[61%] bg-white"></div>
                         <div className="flex flex-col w-full h-auto">
                             <p className="text-lg">Nome da música</p>
