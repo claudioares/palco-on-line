9a0b6a972e3dc391cc7e59302c7f776c95e25718
claudioares <claudiojasoares.dev@gmail.com>
2024-05-31 11:40:09 -0300

    feat:Adicionando sitema de videos

--- src/API_simulation_to_test/API.tsx
+++ src/API_simulation_to_test/API.tsx
@@ -5,7 +5,25 @@ import imgCard4 from "../assets/imgns_videosCards/img_card (4).jpg"
 import imgCard5 from "../assets/imgns_videosCards/img_card (5).jpg"
 import imgCard6 from "../assets/imgns_videosCards/img_card (6).jpg"
 import imgCard7 from "../assets/imgns_videosCards/img_card (7).jpg"
-import { youtubeApi } from "../service/api"
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+export const playlistArr = async () => {
+    const token = process.env.USER_TOKEN;
+    const playlistId = process.env.PLAY_LIST_ID;
+    
+    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${playlistId}&key=${token}`;
+    
+    const response = await fetch(url)
+      .then((response) => response.json())
+      .then(data => {return data})
+      .catch((error) => console.error("Erro:", error));
+
+
+      return response.items;
+}
 
 
 interface IImageMetadata {
--- src/components/cardsVideos/infoMusic.tsx
+++ src/components/cardsVideos/infoMusic.tsx
@@ -2,22 +2,13 @@ import { HandHeart } from "lucide-react";
 
 type infoMusicProps = {
     musicName: string,
-    composer: string,
     interpreter: string,
 }
-export default function InfoMusic ({musicName, composer, interpreter}:infoMusicProps) {
+export default function InfoMusic ({musicName, interpreter}:infoMusicProps) {
     return(
-        <div className="flex flex-col w-full h-auto gap-1 px-2">
+        <div className="flex flex-col w-full h-auto gap-1 px-2 text-white">
             <p className="text-base">{musicName}</p>
             <div className="h-auto w-full flex flex-col text-sm">
-                <div className="flex gap-2">
-                    <p className="font-Inter font-normal opacity-85 text-zinc-400">
-                    Autor(es):
-                    </p>
-                    <p className="font-light text-zinc-200 opacity-85">
-                    {composer}
-                    </p>
-                </div>
                 <div className="flex gap-2">
                     <p className="font-Inter font-normal opacity-85 text-zinc-400">
                     Interprete(es):
